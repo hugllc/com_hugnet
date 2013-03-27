d1a65c6da752b69f25d9b8d995fbe3d8f083019f
Scott Price <prices@hugllc.com>
2013-03-27 13:06:40 -0500

    This is the rest of the files that fix #7366.

---
+++ test/bootstrap.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This component is the user interface for the endpoints
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * com_timeclock is a Joomla! 1.6 component
+ * Copyright (C) 2008-2009, 2011 Hunt Utilities Group, LLC
+ * Copyright 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Test
+ * @package    ComTimeclock
+ * @subpackage Com_Timeclock
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2008-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$
+ * @link       https://dev.hugllc.com/index.php/Project:Comtimeclock
+ */
+
+define('JPATH_BASE', realpath(dirname(__FILE__)."/../src"));
+define('JPATH_ROOT', realpath(dirname(__FILE__)."/../src"));
+define('DS', "/");
+define('JPATH_COMPONENT_SITE', JPATH_ROOT."/frontend");
+define('JPATH_COMPONENT_ADMINISTRATOR', JPATH_ROOT."/backend");
+
+
+?>
